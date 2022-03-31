9c608d477e78b5a705d0358828d2cb6744d842f2
NgTuong24 <99720713+NgTuong24@users.noreply.github.com>
2022-03-31 23:59:54 +0700

    Add files via upload

--- v15_Thresholding.py
+++ v15_Thresholding.py
@@ -19,4 +19,20 @@ cv.imshow("th4", th4)
 cv.imshow("th5", th5)
 cv.waitKey(0)
 cv.destroyAllWindows()
+'''
+THRESH_BINARY: Có thể dịch là ngưỡng nhị phân. Ý nghĩa y hệt những gì mình đề cập ở trên.
 
+THRESH_BINARY_INV: Ngưỡng nhị phân đảo ngược. Có thể hiểu là nó sẽ đảo ngược lại kết quả của THRESH_BINARY.
+
+THRESH_TRUNC: Những giá trị điểm ảnh bé hơn ngưỡng sẽ giữ nguyên giá trị, những điểm ảnh lớn hơn hoặc ngưỡng sẽ được gán lại là maxvalue.
+
+THRESH_TOZERO: Những điểm ảnh bé hơn ngưỡng sẽ bị gán thành 0, những điểm còn lại giữ nguyên.
+
+THRESH_TOZERO_INV: Những điểm ảnh nhỏ hơn giá trị ngưỡng sẽ được giữ nguyên, những điểm ảnh còn lại sẽ bị gán thành 0.
+
+THRESH_MASK: Ở bạn opencv4, hầu như không được xài.
+
+THRESH_OTSU: Sử dụng thuật toán Otsu để xác định giá trị ngưỡng.
+
+THRESH_TRIANGLE: Sử dụng thuật toán Triangle để xác định giá trị ngưỡng.
+'''
